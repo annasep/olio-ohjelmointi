a641a5e13dacb397b0bd23a56c5b6e7502cfebdf
Anna Seppänen <anna-julia.seppanen@hotmail.com>
2024-12-10 13:51:01 +0200

    harjoitus6b

---
+++ harjoitus6b/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include "exampleclass.h"
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+
+    ExampleClass example;
+    example.startToWait();
+
+    return a.exec();
+}
